5ad4775332a79c5979f6c99380b8e452bb631717
Xbetty <betty.xiong@pdabc.com>
2021-11-27 17:22:52 +0800

    feat: watch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,12 +10,13 @@
     </ul>
     <h1>{{person.name}}</h1>
     <button @click="increse">+1</button>
+    <h1 @click="updateGreeting">{{greetings}}</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { ref, computed, reactive, toRefs, onMounted, onUpdated, onRenderTriggered } from 'vue'
+import { ref, computed, reactive, toRefs, onMounted, onUpdated, onRenderTriggered, watch } from 'vue'
 // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 interface DataProps {
   count: number;
@@ -55,6 +56,30 @@ export default defineComponent({
       numbers: [0, 1, 2],
       person: {}
     })
+    const greetings = ref('title')
+    const updateGreeting = () => {
+      greetings.value = 'hello'
+    }
+    // watch(greetings, (newVal, oldVal)=>{
+    //   console.log('newVal, oldVal',newVal, oldVal)
+    //   document.title = 'updated' + greetings.value
+    // })
+    // watch多个值，第一个参数可接收数组，newVal和oldVal都变成了数组
+    // watch([greetings, data], (newVal, oldVal)=>{
+    //   // data的值是proxy
+    //   console.log('newVal', newVal, 'oldVal', oldVal)
+    //   console.log('greetings',greetings)
+    //   console.log('data',data)
+    //   document.title = 'updated' + greetings.value
+    // })
+    // 不监听data的所有对象，只监听单个属性，不可以用data.count，会失去响应式，要用回调函数取
+    watch([greetings, data.count], (newVal, oldVal)=>{
+      // data的值是proxy
+      console.log('newVal', newVal, 'oldVal', oldVal)
+      console.log('greetings',greetings)
+      console.log('data',data)
+      document.title = 'updated' + greetings.value
+    })
     // vue3能响应式更改数组和对象，使$set成为过去时
     data.numbers[0] = 3
     data.person.name="xxx"
@@ -66,7 +91,9 @@ export default defineComponent({
     //   increse
     // }
     return {
-      ...refData
+      ...refData,
+      greetings,
+      updateGreeting
       // 响应式类型在模版中才是响应式的，这里的响应式是ref类型，把值从响应式中取出来就失去了响应式的魔力，变成普通的js类型
       // ...data
     }
